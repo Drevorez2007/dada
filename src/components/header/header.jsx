66d5b0478db78695fe76720513b272c8deceaa0c
Drevorez2007 <danilarim6@gmail.com>
2025-06-23 11:36:11 +0300

    fix

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -5,7 +5,7 @@ import logo from '../../assets/img/logo.svg';
 export default function Header() {
   return (
     <header className="header">
-      <img src={logo} alt="" />
+      <img className="fly" src={logo} alt="" />
     </header>
   );
 }
